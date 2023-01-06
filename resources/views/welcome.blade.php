93355b6424c15f05f53d89c4485f12dbc918ae5f
Jonathan Castro <jonathan.castro@whitejaguars.com>
2023-01-05 22:56:42 -0600

    Responsive video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,7 @@
                         <div class="swiper-slide shadow-xl mb-10 mt-5  rounded-xl ">
                             <div class="flex flex-col  space-y-4">
                                 <div class="rounded-t-xl  group cursor-pointer relative flex justify-center items-center">
-                                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcenmiscr%2Fvideos%2F916516816387262%2F&width=320" width="320" height="240" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcenmiscr%2Fvideos%2F916516816387262%2F&width=320" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
                                 </div>
                                 <div class="px-4 py-6 rounded-b-xl w-full flex justify-between  flex-col items-start ">
                                     <p class="text-sm leading-none text-gray-600">Martes 3 de Enero </p>
