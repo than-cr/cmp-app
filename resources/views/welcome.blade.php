0bd8a8c46fea8ca20979695fe4d42bcfa7e1cebd
Jonathan Castro <jonathan.castro@whitejaguars.com>
2023-01-08 08:31:19 -0600

    New service at church

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,19 @@
                         <div class="swiper-slide shadow-xl mb-10 mt-5  rounded-xl ">
                             <div class="flex flex-col  space-y-4">
                                 <div class="rounded-t-xl  group cursor-pointer relative flex justify-center items-center">
-                                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcenmiscr%2Fvideos%2F916516816387262%2F&width=320" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcenmiscr%2Fvideos%2F709508567329998%2F&width=1080" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                                </div>
+                                <div class="px-4 py-6 rounded-b-xl w-full flex justify-between  flex-col items-start ">
+                                    <p class="text-sm leading-none text-gray-600">Domingo 8 de Enero </p>
+                                    <p class="mt-3 text-base font-semibold leading-none text-gray-800">8:30 am</p>
+                                    <p class="text-sm font-semibold leading-none text-blue-700">Caminando hacia la madurez</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide shadow-xl mb-10 mt-5  rounded-xl ">
+                            <div class="flex flex-col  space-y-4">
+                                <div class="rounded-t-xl  group cursor-pointer relative flex justify-center items-center">
+                                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcenmiscr%2Fvideos%2F916516816387262%2F&width=1080" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
                                 </div>
                                 <div class="px-4 py-6 rounded-b-xl w-full flex justify-between  flex-col items-start ">
                                     <p class="text-sm leading-none text-gray-600">Martes 3 de Enero </p>
