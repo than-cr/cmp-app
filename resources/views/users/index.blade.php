1f30237616ec4b9b1d46b0fbbce02759b988315e
Jonathan Castro <jonathan.castro@whitejaguars.com>
2023-01-11 09:53:02 -0600

    Added more data to users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -70,6 +70,24 @@
 
                             <td class="content-center" style="width: 20%">
                                 <div class="flex items-center pl-5">
+                                    <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Provincia</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
+                                    </svg>
+                                </div>
+                            </td>
+
+                            <td class="content-center" style="width: 20%">
+                                <div class="flex items-center pl-5">
+                                    <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Cantón</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
+                                    </svg>
+                                </div>
+                            </td>
+
+                            <td class="content-center" style="width: 20%">
+                                <div class="flex items-center pl-5">
                                     <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Distrito</p>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
@@ -152,6 +170,18 @@
                                 <td class="">
                                     <div class="flex items-center pl-5">
                                         <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">{!! ucfirst(\Carbon\Carbon::createFromDate($user->birthDate)->translatedFormat('d M Y')) !!}</p>
+                                    </div>
+                                </td>
+
+                                <td class="">
+                                    <div class="flex items-center pl-5">
+                                        <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">{{ $user->district->canton->province->name }}</p>
+                                    </div>
+                                </td>
+
+                                <td class="">
+                                    <div class="flex items-center pl-5">
+                                        <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">{{ $user->district->canton->name }}</p>
                                     </div>
                                 </td>
 
