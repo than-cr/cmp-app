36cf1ce38932b20d24c5dfca41d5f174fd095cf6
Jonathan Castro <jonathan.castro@whitejaguars.com>
2023-01-15 13:20:02 -0600

    Fix on register

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,9 +44,9 @@
                                     </x-slot>
 
                                     <x-slot name="content">
-                                        <x-dropdown-link :href="route('profile.edit')">
-                                            {{ __('Perfil') }}
-                                        </x-dropdown-link>
+{{--                                        <x-dropdown-link :href="route('profile.edit')">--}}
+{{--                                            {{ __('Perfil') }}--}}
+{{--                                        </x-dropdown-link>--}}
 
                                         <!-- Authentication -->
                                         <form method="POST" action="{{ route('logout') }}">
@@ -96,11 +96,11 @@
                         </x-responsive-nav-link>
                     </div>
 
-                    <div class="pt-2 pb-3 space-y-1">
-                        <x-responsive-nav-link :href="route('profile.edit')">
-                            {{ __('Perfil') }}
-                        </x-responsive-nav-link>
-                    </div>
+{{--                    <div class="pt-2 pb-3 space-y-1">--}}
+{{--                        <x-responsive-nav-link :href="route('profile.edit')">--}}
+{{--                            {{ __('Perfil') }}--}}
+{{--                        </x-responsive-nav-link>--}}
+{{--                    </div>--}}
 
                     <div class="pt-2 pb-3 space-y-1">
                         <form method="POST" action="{{ route('logout') }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,7 +129,7 @@
                                         <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcenmiscr%2Fvideos%{{$live->live_id}}%2F&width=1080" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
                                     </div>
                                     <div class="px-4 py-6 rounded-b-xl w-full flex justify-between  flex-col items-start ">
-                                        <p class="text-sm leading-none text-gray-600">{!! ucfirst(\Carbon\Carbon::createFromDate($live->date)->translatedFormat('l d M')) !!}</p>
+                                        <p class="text-sm leading-none text-gray-600">{!! ucfirst(\Carbon\Carbon::createFromDate($live->date)->translatedFormat('l d M Y')) !!}</p>
                                         <p class="mt-3 text-base font-semibold leading-none text-gray-800">{!! \Carbon\Carbon::createFromTimeString($live->time)->format('g:i A') !!}</p>
                                         <p class="text-sm font-semibold leading-none text-blue-700">{{$live->name}}</p>
                                     </div>
