13667634c5391f33964c6f54d43fa8cf4d544bdc
Jonathan Castro <jonathan.castro@whitejaguars.com>
2023-01-16 14:31:18 -0600

    Responsive design

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,25 +17,7 @@
 
                                 <td class="text-center">
                                     <div class="flex items-center pl-5">
-                                        <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Nombre</p>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-lock-fill" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z"/>
-                                        </svg>
-                                    </div>
-                                </td>
-
-                                <td class="text-center">
-                                    <div class="flex items-center pl-5">
-                                        <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Apellido</p>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-lock-fill" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z"/>
-                                        </svg>
-                                    </div>
-                                </td>
-
-                                <td class="text-center">
-                                    <div class="flex items-center pl-5">
-                                        <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Apellido</p>
+                                        <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Nombre Completo</p>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-lock-fill" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z"/>
                                         </svg>
@@ -71,7 +53,7 @@
 
                                 <td class="content-center" style="width: 20%">
                                     <div class="flex items-center pl-5">
-                                        <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Asiste a Gedeón</p>
+                                        <p class="text-base font-medium leading-none text-blue-700 dark:text-white  mr-2">Gedeón</p>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
                                         </svg>
@@ -140,19 +122,7 @@
 
                                     <td class="">
                                         <div class="flex items-center pl-5">
-                                            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">{{ $user->name }}</p>
-                                        </div>
-                                    </td>
-
-                                    <td class="">
-                                        <div class="flex items-center pl-5">
-                                            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">{{ $user->lastName }}</p>
-                                        </div>
-                                    </td>
-
-                                    <td class="">
-                                        <div class="flex items-center pl-5">
-                                            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">{{ $user->motherLastName }}</p>
+                                            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">{{ $user->name }} {{ $user->lastName }} {{ $user->motherLastName }}</p>
                                         </div>
                                     </td>
 
@@ -275,7 +245,6 @@
                             </tbody>
                         </table>
                     </div>
-
                     <br>
                 </div>
             </div>
