809acdf699a4762f8cfe92af8495142c184a0700
Jonathan Castro <jonathan.castro@whitejaguars.com>
2023-01-22 08:29:29 -0600

    Pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(15);
         return view('users.index', compact('users'));
     }
 
