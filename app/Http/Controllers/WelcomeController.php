48a5bfa826ede5a85f2d4dd7b7a0babec0f860aa
Jonathan Castro <than.cr@outlook.com>
2025-01-05 10:57:02 -0600

    Fixed lives

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Live;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class WelcomeController extends Controller
 {
-    //
-    public function index()
+    /**
+     * Show the application welcome screen.
+     *
+     * @return Response
+     */
+    public function index(): Response
     {
         $lives = Live::orderBy('id', 'DESC')->take(10)->get();
         return view('welcome', compact('lives'));
